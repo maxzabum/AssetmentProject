c5f30d86e5b6a1f198c101d0547e36b0cedd9296
maxzabum <maxzabum1150@hotmail.com>
2020-01-26 05:38:19 +0700

    push

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import * as serviceWorker from "./serviceWorker";
-import Amplify from "aws-amplify";
-import config from "./aws-exports";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 ReactDOM.render(
