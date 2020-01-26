68bf64da52e10e4aeff929fc17647670513a6a61
maxzabum <maxzabum1150@hotmail.com>
2020-01-27 06:36:10 +0700

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 mongoose
-  .connect(process.env.MONGODB_URI || db, {
+  .connect(db, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useCreateIndex: true
