03c938bb4efe24b66166bd8c1ffce0f0c0cded3b
maxzabum <maxzabum1150@hotmail.com>
2020-01-27 06:35:05 +0700

    Update server.js

--- server.js
+++ server.js
@@ -43,7 +43,9 @@ app.use("/uploads", express.static("uploads"));
 
 //SERV static assets
 if (process.env.NODE_ENV === "production") {
+  // Set static folder
   app.use(express.static("client/build"));
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
