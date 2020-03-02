229a950c9ffb5eee64b9c8d84bf0104386e9e874
maxzabum <maxzabum1150@hotmail.com>
2020-03-02 20:55:53 +0700

    make it better

--- client/src/store.js
+++ client/src/store.js
@@ -7,6 +7,10 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
+  // compose(
+  //   applyMiddleware(...middleware),
+  //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // )
   compose(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
