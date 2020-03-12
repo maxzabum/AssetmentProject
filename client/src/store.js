ed5b0da36eeb4742cdcfab32c25ac119da518316
maxzabum <maxzabum1150@hotmail.com>
2020-03-13 02:16:59 +0700

    make it better

--- client/src/store.js
+++ client/src/store.js
@@ -4,13 +4,19 @@ import rootReducer from "./reducers";
 
 const initialState = {};
 const middleware = [thunk];
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   // compose(
+//   //   applyMiddleware(...middleware),
+//   //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   // )
+//   compose(applyMiddleware(...middleware))
+// );
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
   initialState,
-  // compose(
-  //   applyMiddleware(...middleware),
-  //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  // )
-  compose(applyMiddleware(...middleware))
+  /* preloadedState, */ composeEnhancers(applyMiddleware(...middleware))
 );
 export default store;
